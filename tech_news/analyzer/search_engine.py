f53cc2e34a001d60481db24df7b52276728d5d56
Paula <paulasalinor@gmail.com>
2022-11-08 18:05:01 -0300

    feat: add search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,11 @@
-# Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in news]
 
 
-# Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
 
@@ -16,3 +18,7 @@ def search_by_tag(tag):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    print(search_by_title("BACANA"))
