5d8be5f11d065dfe7a0988f4e801e69c0f56ab4e
Paula <paulasalinor@gmail.com>
2022-11-08 16:23:51 -0300

    feat: add scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,9 +29,28 @@ def scrape_next_page_link(html_content):
     return next_page
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    comments_count = selector.css(".post-comments-simple h5::text").get() or 0
+    summary = selector.xpath("string(//p)").get().strip()
+    tags = selector.css(".post-tags a::text").getall()
+    category = selector.css(".meta-category span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
@@ -40,6 +59,9 @@ def get_tech_news(amount):
 
 
 if __name__ == "__main__":
-    html_content = fetch("https://blog.betrybe.com/")
+    html_content = fetch(
+        "https://blog.betrybe.com/noticias/bill-gates-e-cetico-sobre-criptomoedas-e-nfts-entenda-o-motivo/"
+    )
     # print(scrape_novidades(html_content))
     # print(scrape_next_page_link(html_content))
+    print(scrape_noticia(html_content))
