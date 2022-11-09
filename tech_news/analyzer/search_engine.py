54efca3a5c671724767c4b1e00da0bfd8404297c
Paula <paulasalinor@gmail.com>
2022-11-09 05:10:07 -0300

    feat: add search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 def search_by_title(title):
@@ -7,18 +8,25 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_formmat = datetime.datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+        news = search_news({"timestamp": {"$regex": date_formmat}})
+        return [(new["title"], new["url"]) for new in news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
-# Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    pass
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    pass
 
 
-if __name__ == "__main__":
-    print(search_by_title("BACANA"))
+# if __name__ == "__main__":
+#     print(search_by_title("BACANA"))
+#     search_by_date("2022-04-07")
+#     search_by_date(["Tecnologia", "TECNOLOGIA", "Esportes", "Aloha"])
